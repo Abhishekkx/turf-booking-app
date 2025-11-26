71048f1f4b2ea7d8f17516ab95454665d555b1f2
Abhishek <aabhishekk920@gmail.com>
2025-11-26 16:18:13 +0530

    Add PlatformConstants polyfill for Snack compatibility

--- App.js
+++ App.js
@@ -1,7 +1,32 @@
 import React from 'react';
-import { StatusBar } from 'react-native';
+import { StatusBar, Platform } from 'react-native';
 import { BookingProvider } from './src/context/BookingContext';
 import AppNavigator from './src/navigation/AppNavigator';
+
+// Polyfill for Snack compatibility
+if (typeof global.HermesInternal === 'undefined') {
+  try {
+    const { TurboModuleRegistry } = require('react-native');
+    if (TurboModuleRegistry && !TurboModuleRegistry.get('PlatformConstants')) {
+      TurboModuleRegistry.get = (name) => {
+        if (name === 'PlatformConstants') {
+          return {
+            getConstants: () => ({
+              isTesting: false,
+              reactNativeVersion: { major: 0, minor: 72, patch: 0 },
+              forceTouchAvailable: false,
+              osVersion: Platform.Version,
+              systemName: Platform.OS,
+            }),
+          };
+        }
+        return null;
+      };
+    }
+  } catch (e) {
+    // Ignore polyfill errors
+  }
+}
 
 export default function App() {
   return (
